679782ee44ef3573d01121e35e3a1f1fae914668
Pablo Piñeiro <pmpv05@hotmail.com>
2019-01-21 16:25:50 -0300

    Cambios

--- INDDHH Eclipse/javaSourceProject/inddhh/SetParamsToExpediente.java
+++ INDDHH Eclipse/javaSourceProject/inddhh/SetParamsToExpediente.java
@@ -53,6 +53,46 @@ public class SetParamsToExpediente extends ApiaAbstractClass {
 		entDefTramite.getAttribute("DEF_TRM_EXPEDIENTE_ADJUNTO_STR").setValue(adjunto);
 		entDefTramite.getAttribute("DEF_TRM_CONCATENAR_PDF_STR").setValue(pdf);
 		
+		//Archivo Estructurado: XML donde le paso los atributos Nombre, Apellido, Doc, etc...
+		Entity currEnt = this.getCurrentEntity();
+		
+		//Datos personales
+		String tipoDoc = currEnt.getAttribute("INDDHH_ATT_DATOS_PERSONALES_DOC_TIPO_STR").getValueAsString();
+		String doc = currEnt.getAttribute("INDDHH_ATT_DATOS_PERSONALES_DOC_NUM_STR").getValueAsString();
+		String paisEmisor = currEnt.getAttribute("INDDHH_ATT_DATOS_PERSONALES_PAIS_STR").getValueAsString();
+		String nombre = currEnt.getAttribute("INDDHH_ATT_DATOS_PERSONALES_NOMBRES_STR").getValueAsString();
+		String apellido = currEnt.getAttribute("INDDHH_ATT_DATOS_PERSONALES_APELLIDOS_STR").getValueAsString();
+		String nacionalidad = currEnt.getAttribute("INDDHH_NACIONALIDAD_STR").getValueAsString();
+		String genero = currEnt.getAttribute("INDDHH_PERSONA_GENERO_STR").getValueAsString();
+		String grupoEtnico = currEnt.getAttribute("INDDHH_GRUPO_ETNICO_STR").getValueAsString();
+		String fechaNac = currEnt.getAttribute("INDDHH_PERSONA_FISICA_FECHA_NACIMIENTO_DTE").getValueAsString();
+		String edad = currEnt.getAttribute("INDDHH_EDAD_NUM").getValueAsString();
+		
+		//Domicilio
+		String depto = currEnt.getAttribute("INDDHH_ATT_DOMICILIO_DEPTO_STR").getValueAsString();
+		String loc = currEnt.getAttribute("INDDHH_ATT_DOMICILIO_LOCALIDAD_STR").getValueAsString();
+		String calle = currEnt.getAttribute("INDDHH_ATT_DOMICILIO_CALLE_STR").getValueAsString();
+		String numero = currEnt.getAttribute("INDDHH_ATT_DOMICILIO_NUMERO_STR").getValueAsString();
+		String otrosDatos = currEnt.getAttribute("INDDHH_ATT_DOMICILIO_OTROS_DATOS_STR").getValuesAsString();
+		
+		//Contacto
+		String cel = currEnt.getAttribute("INDDHH_TELEFONO_CONTACTO_STR").getValueAsString();
+		String tel = currEnt.getAttribute("INDDHH_OTRO_TEL_CONTACTO_STR").getValueAsString();
+		String correo = currEnt.getAttribute("INDDHH_CORREO_CONTACTO_STR").getValueAsString();
+		
+		//Descripción del problema
+		String problema = currEnt.getAttribute("INDDHH_PROBLEMA_PERSONA_STR").getValuesAsString();
+		String fechaInicio = currEnt.getAttribute("INDDHH_FECHA_INICIO_HECHOS_STR").getValueAsString();
+		String fechaFin = currEnt.getAttribute("INDDHH_FECHA_FIN_HECHOS_STR").getValueAsString();
+		String casoJusticia = currEnt.getAttribute("INDDHH_CASO_ORBITA_JUSTICIA_STR").getValueAsString();
+		
+		//Organismos denunciados
+		String orgs = currEnt.getAttribute("INDDHH_ORG_INCISO_TABLA_STR").getValueAsString();
+		String unEjs = currEnt.getAttribute("INDDHH_ORG_UNIDAD_EJECUTORA_TABLA_STR").getValueAsString();
+		
+		String xml = "";
+		entDefTramite.getAttribute("TRM_XML_FILES_STR").setValueLargeStr(xml);
+		
 	}
 
 }
